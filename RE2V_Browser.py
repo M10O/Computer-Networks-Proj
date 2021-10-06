1980ca3612bfaf4bb3eacef35f999da9a4cb8317
M10O <73187017+M10O@users.noreply.github.com>
2021-10-06 13:56:43 +0530

    Add files via upload

--- RE2V_Browser.py
+++ RE2V_Browser.py
@@ -1,7 +1,7 @@
 import socket
 new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 new_socket.connect(("google.com", 80))
-cmd = 'GET http://www.google.com/1.0\r\n\n\'.encode()
+cmd = 'GET http://www.google.com/\r\n\r\n\'.encode()
 new_socket.send(cmd)
 
 while True:
